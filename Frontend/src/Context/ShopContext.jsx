e0688eb0bb544776f3f3b017045ae4c7c1389bfe
codeculture983-cpu <codeculture983@gmail.com>
2026-05-05 07:11:07 -0700

    Update backend URL in ShopContext

--- Frontend/src/Context/ShopContext.jsx
+++ Frontend/src/Context/ShopContext.jsx
@@ -10,7 +10,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const delivery_fee = 10;
-  const backend_url = import.meta.env.VITE_BACKEND_URL;
+  const backend_url = "https://forever-backend-f3kf.onrender.com";
 
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
