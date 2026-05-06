719b05dabf552c673b4787239b3493c8002c4114
codeculture983-cpu <codeculture983@gmail.com>
2026-05-06 03:26:43 -0700

    Update backend URL to production server

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -15,7 +15,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl =
-  "http://localhost:4000";
+  "https://forver-backend.onrender.com";
 
 export const currency = "$";
 
